15e19207c3793bd687491ac36486453e810f2c01
Colin van Dercreek <c.p.vandercreek-1@tudelft.nl>
2021-10-05 10:41:25 +0200

    I added a function to test the git pull request

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
 
+def print_pleasework(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, I hope this works {name}')  #
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
