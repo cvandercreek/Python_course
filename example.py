978b87a9e644ecf9cbd435f9130980d730b8445e
cvandercreek <42575134+cvandercreek@users.noreply.github.com>
2021-10-07 12:32:18 +0200

    Update example.py

--- example.py
+++ example.py
@@ -9,6 +9,67 @@ def test_add():
 def subtract(a, b):
     return a - b  # <--- fix this 
 
+#1
+def factorial(n):
+    """
+    Computes the factorial of n.
+    """
+    if n < 0:
+        raise ValueError('received negative input')
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+
+# 2
+def count_word_occurrence_in_string(text, word):
+    """
+    Counts how often word appears in text.
+    Example: if text is "one two one two three four"
+             and word is "one", then this function returns 2
+    """
+    words = text.split()
+    return words.count(word)
+
+
+# 3
+def count_word_occurrence_in_file(file_name, word):
+    """
+    Counts how often word appears in file file_name.
+    Example: if file contains "one two one two three four"
+             and word is "one", then this function returns 2
+    """
+    count = 0
+    with open(file_name, 'r') as f:
+        for line in f:
+            words = line.split()
+            count += words.count(word)
+    return count
+
+
+# 4
+def check_reactor_temperature(temperature_celsius):
+    """
+    Checks whether temperature is above max_temperature
+    and returns a status.
+    """
+    from reactor import max_temperature
+    if temperature_celsius > max_temperature:
+        status = 1
+    else:
+        status = 0
+    return status
+
+
+# 5
+class Pet:
+    def __init__(self, name):
+        self.name = name
+        self.hunger = 0
+    def go_for_a_walk(self):  # <-- how would you test this function?
+        self.hunger += 1
+
 
 # uncomment the following test
 def test_subtract():
