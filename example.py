7bc482aec088482ba64b68bba9b79d8e540415af
cvandercreek <42575134+cvandercreek@users.noreply.github.com>
2021-10-07 12:22:07 +0200

    Update example.py

--- example.py
+++ example.py
@@ -5,3 +5,11 @@ def add(a, b):
 def test_add():
     assert round(add(.1, .2),4) == .3
     assert add('space', 'ship') == 'spaceship'
+    
+def subtract(a, b):
+    return a + b  # <--- fix this 
+
+
+# uncomment the following test
+def test_subtract():
+    assert subtract(2, 3) == -1
